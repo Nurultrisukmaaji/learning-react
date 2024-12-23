1fbc3a46e99cba85949e4bf91afb59f00323609a
Nurultrisukmaaji <nurultrisukmaaji@gmail.com>
2024-12-23 12:02:13 +0700

    update

--- src/components/item-list2.jsx
+++ src/components/item-list2.jsx
@@ -1,6 +1,14 @@
+import { useState } from "react";
+
 export default function ItemListNew({items, type}){
     if(!items.length){
         <p>No Items Available</p>
+    }
+
+    const [isVisible, setIsVisible] = useState(true)
+
+    const toggleCard= () => {
+        setIsVisible(!isVisible);
     }
 
     return items.map((item) => {
@@ -10,6 +18,18 @@ export default function ItemListNew({items, type}){
                     <>
                         <h2>{item.title}</h2>
                         <p>{item.body}</p>
+                        <br />
+                        {isVisible && (
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                                Praesentium, architecto totam iure beatae nulla laudantium blanditiis, 
+                                maxime eveniet incidunt minus quos consectetur ipsam distinctio 
+                                illum ex officiis officia magnam nobis.
+                            </p>
+                        )}
+                        <button onClick={toggleCard}>
+                            Read More
+                        </button>
                     </>
                 )}
                 {type === "comment" && (
