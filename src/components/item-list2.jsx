2acaf4f8a3acbed514d078978415aaaf59a2a39d
Nurultrisukmaaji <nurultrisukmaaji@gmail.com>
2024-12-23 12:50:27 +0700

    update

--- src/components/item-list2.jsx
+++ src/components/item-list2.jsx
@@ -1,14 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
+import SlideToggle from "./slide-toggle";
 
 export default function ItemListNew({items, type}){
     if(!items.length){
         <p>No Items Available</p>
     }
 
-    const [isVisible, setIsVisible] = useState(true)
+    const [isVisible, setIsVisible] = useState([]);
 
-    const toggleCard= () => {
-        setIsVisible(!isVisible);
+    const toggleCard = (id) => {
+        // setIsVisible(isVisible === id ? null : id);
+
+        if(isVisible.includes(id)){
+            setIsVisible(isVisible.filter((cardId) => cardId !== id));
+        } else {
+            setIsVisible([...isVisible, id]);
+        }
     }
 
     return items.map((item) => {
@@ -19,7 +26,7 @@ export default function ItemListNew({items, type}){
                         <h2>{item.title}</h2>
                         <p>{item.body}</p>
                         <br />
-                        {isVisible && (
+                        {isVisible.includes(item.id) &&  (
                             <p>
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                                 Praesentium, architecto totam iure beatae nulla laudantium blanditiis, 
@@ -27,7 +34,7 @@ export default function ItemListNew({items, type}){
                                 illum ex officiis officia magnam nobis.
                             </p>
                         )}
-                        <button onClick={toggleCard}>
+                        <button onClick={() => toggleCard(item.id)}>
                             Read More
                         </button>
                     </>
@@ -36,6 +43,18 @@ export default function ItemListNew({items, type}){
                     <>
                         <h2>{item.name}</h2>
                         <p>{item.email}</p>
+                        <br />
+                        {isVisible.includes(item.id) &&  (
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                                Praesentium, architecto totam iure beatae nulla laudantium blanditiis, 
+                                maxime eveniet incidunt minus quos consectetur ipsam distinctio 
+                                illum ex officiis officia magnam nobis.
+                            </p>
+                        )}
+                        <button onClick={() => toggleCard(item.id)}>
+                           {isVisible.includes(item.id) ? "Read Less" : "Read More"}
+                        </button>
                     </>
                 )}
                 {type === "user" && (
